a210df96cb3c807825a61a0d90348789579203b4
sophie1001 <sophiechung1001@gmail.com>
2024-09-07 16:32:20 +0000

    convert to spectogram

--- main.py
+++ main.py
@@ -9,3 +9,25 @@ if audio_file:
     with open(AUDIO_FILE_NAME, "wb") as f:
         f.write(audio_file.getbuffer())
         st.toast("Audio Reading Successful", icon = "✔")
+
+# Function to convert the audio waveform to spectrogram
+def audio_to_spectrogram(y, sr):
+    # Generate a spectrogram
+    spectrogram = librosa.feature.melspectrogram(y = y, sr = sr)
+
+    # Convert to decibels
+    log_spectrogram = librosa.power_to_db(spectrogram, ref=np.max)
+
+    # Plot the spectrogram
+    plt.figure(figsize=(10, 6))
+    plt.axis('off')
+    librosa.display.specshow(log_spectrogram, sr=sr)
+
+    # Save the spectrogram if a save_path is provided
+    plt.tight_layout()
+    plt.savefig("test.png")
+    plt.show()
+    
+y, sr = librosa.load(audio_path)
+audio_to_spectrogram(y, sr)
+spectrogram = "test.png"
