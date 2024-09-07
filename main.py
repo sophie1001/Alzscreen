ce6a2bffe4fbef6c619caa0545b8e770b2d714a3
sophie1001 <sophiechung1001@gmail.com>
2024-09-07 16:22:08 +0000

    define audio file (error message)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+AUDIO_FILE_NAME = "uploaded_audio.wav"
 st.title('Alzscreen')
 # upload the audio file
 audio_file = st.file_uploader("Upload an audio file", type = ["wav", "mp3"], help = "Upload the audio file of the patient.")
