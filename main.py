562ea929e609cd5a79ac776ea94f9272f37bcb56
sophie1001 <sophiechung1001@gmail.com>
2024-09-07 16:16:28 +0000

    upload audio file

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 import streamlit as st
 st.title('Alzscreen')
+# upload the audio file
+audio_file = st.file_uploader("Upload an audio file", type = ["wav", "mp3"], help = "Upload the audio file of the patient.")
