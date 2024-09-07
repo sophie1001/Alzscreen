f633b46c9ccfee0b7ee0b1d0a4399c3772c5c792
sophie1001 <sophiechung1001@gmail.com>
2024-09-07 16:20:25 +0000

    save audio file

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import streamlit as st
 st.title('Alzscreen')
 # upload the audio file
 audio_file = st.file_uploader("Upload an audio file", type = ["wav", "mp3"], help = "Upload the audio file of the patient.")
+
+if audio_file:
+    # Save the uploaded file
+    with open(AUDIO_FILE_NAME, "wb") as f:
+        f.write(audio_file.getbuffer())
+        st.toast("Audio Reading Successful", icon = "")
