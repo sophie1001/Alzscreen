42dd3e94d49cbc0e8407ecb0a2f04d5255cbf004
sophie1001 <sophiechung1001@gmail.com>
2024-09-13 03:08:27 +0000

    cleanining up code

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import librosa
-import librosa.display
-import matplotlib.pyplot as plt
-import numpy as np
-import requests
-import base64
-
-import streamlit as st
 AUDIO_FILE_NAME = "uploaded_audio.wav"
 st.title('Alzscreen')
 # upload the audio file
@@ -16,24 +8,6 @@ if audio_file:
     with open(AUDIO_FILE_NAME, "wb") as f:
         f.write(audio_file.getbuffer())
         st.toast("Audio Reading Successful", icon = "✔")
-
-# Function to convert the audio waveform to spectrogram
-def audio_to_spectrogram(y, sr):
-    # Generate a spectrogram
-    spectrogram = librosa.feature.melspectrogram(y = y, sr = sr)
-
-    # Convert to decibels
-    log_spectrogram = librosa.power_to_db(spectrogram, ref=np.max)
-
-    # Plot the spectrogram
-    plt.figure(figsize=(10, 6))
-    plt.axis('off')
-    librosa.display.specshow(log_spectrogram, sr=sr)
-
-    # Save the spectrogram if a save_path is provided
-    plt.tight_layout()
-    plt.savefig("test.png")
-    plt.show()
     
 y, sr = librosa.load(AUDIO_FILE_NAME)
 audio_to_spectrogram(y, sr)
---
+++ utils.py
@@ -0,0 +1,28 @@
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+import requests
+import base64
+import streamlit as st
+
+# Function to convert the audio waveform to spectrogram
+def audio_to_spectrogram(audio_file_path: str):
+    try:
+        # Load the audio file
+        y, sr = librosa.load(audio_file_path)
+        # Generate a spectrogram
+        spectrogram = librosa.feature.melspectrogram(y = y, sr = sr)
+        # Convert to decibels
+        log_spectrogram = librosa.power_to_db(spectrogram, ref=np.max)
+        # Plot the spectrogram
+        plt.figure(figsize=(10, 6))
+        plt.axis('off')
+        librosa.display.specshow(log_spectrogram, sr=sr)
+        plt.tight_layout()
+        plt.savefig(IMAGE_NAME)
+        plt.close()
+        return True
+    except Exception as error:
+        print(str(error))
+        return False
